a4a9022ac1f311e1cc046887c71e1f7ea35bd497
Sylvain Joubert <joubert.sy@gmail.com>
2015-03-30 23:21:53 +0200

    Add command line argument handling to server and gui

--- source/gui/main.cpp
+++ source/gui/main.cpp
@@ -1,9 +1,54 @@
 #include "psvPensieveWindow.hpp"
 
+#include "psvConfigure.hpp"
+#include <boost/program_options.hpp>
 #include <QApplication>
+#include <iostream>
 
+/**
+ * Entry point of the graphical application
+ * \param p_argc Parameters count
+ * \param p_argv Parameters values
+ * \return Exit code
+ */
 int main(int p_argc, char** p_argv)
 {
+  // Command line arguments
+  namespace bpo = boost::program_options;
+
+  bpo::options_description options(
+    QObject::tr("Usage: qpensieve [Options] [Qt application options]\nOptions")
+    .toStdString());
+  options.add_options()
+    ("help,h", QObject::tr("Display help message").toStdString().c_str())
+    ("version,v", QObject::tr("Display version").toStdString().c_str());
+
+  bpo::variables_map values;
+  try
+  {
+    auto parsed = bpo::command_line_parser(p_argc, p_argv).options(options)
+      .allow_unregistered().run();
+    bpo::store(parsed, values);
+    bpo::notify(values);
+  }
+  catch(std::exception const& e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl << options;
+    return 1;
+  }
+
+  if(values.count("help"))
+  {
+    std::cout << options;
+    return 0;
+  }
+  if(values.count("version"))
+  {
+    std::cout << psv::PENSIEVE_VERSION << std::endl;
+    return 0;
+  }
+
+  // Start application
   QApplication app(p_argc, p_argv);
 
   psv::PensieveWindow window;
--- source/server/main.cpp
+++ source/server/main.cpp
@@ -1,12 +1,54 @@
 #include "psvServer.hpp"
 
+#include "psvConfigure.hpp"
+#include <boost/program_options.hpp>
+#include <iostream>
+
 /**
  * Entry point of the server executable
+ * \param p_argc Parameters count
+ * \param p_argv Parameters values
+ * \return Exit code
  */
-int main(void)
+int main(int p_argc, char** p_argv)
 {
+  // Command line arguments
+  namespace bpo = boost::program_options;
+  unsigned int port;
+
+  bpo::options_description options("Usage: pensieved [Options]\nOptions");
+  options.add_options()
+    ("help,h", "Display help message")
+    ("version,v", "Display version")
+    ("port,p", bpo::value<unsigned int>(&port)->default_value(7142),
+      "Port to listen to");
+
+  bpo::variables_map values;
+  try
+  {
+    bpo::store(bpo::parse_command_line(p_argc, p_argv, options), values);
+    bpo::notify(values);
+  }
+  catch(std::exception const& e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl << options;
+    return 1;
+  }
+
+  if(values.count("help"))
+  {
+    std::cout << options;
+    return 0;
+  }
+  if(values.count("version"))
+  {
+    std::cout << psv::PENSIEVE_VERSION << std::endl;
+    return 0;
+  }
+
+  // Start application
   psv::Server server;
-  server.Run(7142);
+  server.Run(port);
 
   return 0;
 }
